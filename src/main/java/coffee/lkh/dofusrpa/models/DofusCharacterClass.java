10bc0d9a644525a438fff05d6b94347bbb834431
nakira974 <maxime.loukhal@gmail.com>
2023-06-04 23:17:09 +0200

    Fixed xml serialization

--- src/main/java/coffee/lkh/dofusrpa/models/DofusCharacterClass.java
+++ src/main/java/coffee/lkh/dofusrpa/models/DofusCharacterClass.java
@@ -1,51 +1,27 @@
 package coffee.lkh.dofusrpa.models;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.*;
 
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlRootElement(name = "dofus_character_class")
+
 public enum DofusCharacterClass {
-    @XmlElement(name = "undefined")
     UNDEFINED,
-    @XmlElement(name = "ecaflip")
     ECAFLIP,
-    @XmlElement(name = "eniripsa")
     ENIRIPSA,
-    @XmlElement(name = "iop")
     IOP,
-    @XmlElement(name = "cra")
     CRA,
-    @XmlElement(name = "feca")
     FECA,
-    @XmlElement(name = "sacrier")
     SACRIER,
-    @XmlElement(name = "sadida")
     SADIDA,
-    @XmlElement(name = "osamodas")
     OSAMODAS,
-    @XmlElement(name = "enutrof")
     ENUTROF,
-    @XmlElement(name = "sram")
     SRAM,
-    @XmlElement(name = "xelor")
     XELOR,
-    @XmlElement(name = "pandawa")
     PANDAWA,
-    @XmlElement(name = "rogue_ruse")
     ROGUE_RUSE,
-    @XmlElement(name = "masqueraider_mystique")
     MASQUERAIDER_MYSTIQUE,
-    @XmlElement(name = "foggernauts_steam")
     FOGGERNAUTS_STEAM,
-    @XmlElement(name = "eliotrope")
     ELIOTROPE,
-    @XmlElement(name = "huppermage")
     HUPPERMAGE,
-    @XmlElement(name = "ouginak")
     OUGINAK,
-    @XmlElement(name = "forgelance_heritage")
     FORGELANCE_HERITAGE
 }
--- src/main/java/coffee/lkh/dofusrpa/webservices/implemantations/DofusAccountService.java
+++ src/main/java/coffee/lkh/dofusrpa/webservices/implemantations/DofusAccountService.java
@@ -1,14 +1,19 @@
 package coffee.lkh.dofusrpa.webservices.implemantations;
 
-import coffee.lkh.dofusrpa.models.entities.DofusAccount;
+import coffee.lkh.dofusrpa.models.DofusCharacterClass;
+import coffee.lkh.dofusrpa.models.dto.DofusCharacterDto;
 import coffee.lkh.dofusrpa.models.dto.DofusAccountDto;
+import coffee.lkh.dofusrpa.models.entities.DofusAccount;
 import coffee.lkh.dofusrpa.repositories.IDofusAccountRepository;
 import coffee.lkh.dofusrpa.webservices.IDofusAccountService;
 import jakarta.inject.Inject;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 import jakarta.xml.soap.SOAPException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @WebService(endpointInterface = "coffee.lkh.dofusrpa.webservices.IDofusAccountService",
@@ -17,6 +22,31 @@ public class DofusAccountService implements IDofusAccountService {
 
     @Inject
     private IDofusAccountRepository dofusAccountRepository;
+
+    @Contract(" -> new")
+    private @NotNull ArrayList<DofusAccountDto> getTestUsers(){
+
+        return new ArrayList<>() {{
+            add(
+                    new DofusAccountDto("maxime.loukhal@gmail.com", "54gff65g4f65",
+
+                                    new ArrayList<>() {{
+                                        add(new DofusCharacterDto("Ah_Mon_Gars", DofusCharacterClass.PANDAWA))
+                                        ;
+                                    }}
+                    ));
+        } {{
+            add(
+                    new DofusAccountDto("anthony.mereu77@gmail.com", "54gff65g4f65",
+
+                            new ArrayList<>() {{
+                                add(new DofusCharacterDto("Ah_Mon_Soin", DofusCharacterClass.ENIRIPSA))
+                                ;
+                            }}
+                    ));
+        }}};
+    }
+
 
     /**
      * @param limit
@@ -30,7 +60,9 @@ public class DofusAccountService implements IDofusAccountService {
             if(dofusAccountRepository == null) {
                 throw new SOAPException("Database context is null!");
             }
+
             return dofusAccountRepository.getAll().stream().map(DofusAccount::toDto).toList();
+
         }catch (Exception ex){
             throw new SOAPException("Database context injection Exception", ex);
         }
